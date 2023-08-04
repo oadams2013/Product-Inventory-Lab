0f530b29475e58eb6048bd144570f1584fe3cd76
olamide <oadams@fandm.edu>
2023-08-04 16:31:31 -0400

    more updates

--- src/main/java/Models/Sneakers.java
+++ src/main/java/Models/Sneakers.java
@@ -1,4 +1,12 @@
 package Models;
 
 public class Sneakers {
+
+        private int id;
+        private String name;
+        private String brand;
+        private String sport;
+        private int size;
+        private int qty;
+        private float price;
 }
